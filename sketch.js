772365b9c43c9b4753deb6b48e832c4e5bf378e7
AbdulcaderNhema <abdulcader.nt075@s.msumain.edu.ph>
2026-05-24 00:47:23 +0800

    Tic Tac Toe

---
+++ sketch.js
@@ -0,0 +1,125 @@
+let board = [
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', '']
+];
+
+let w; // Width of each cell
+let h; // Height of each cell
+
+let ai = 'X';
+let human = 'O';
+let currentPlayer = human;
+
+function setup() {
+  createCanvas(400, 400);
+  w = width / 3;
+  h = height / 3;
+  
+  // If AI goes first, call bestMove here. Otherwise, wait for human click.
+  // bestMove(); 
+}
+
+function equals3(a, b, c) {
+  return a == b && b == c && a != '';
+}
+
+function checkWinner() {
+  let winner = null;
+
+  // Horizontal
+  for (let i = 0; i < 3; i++) {
+    if (equals3(board[i][0], board[i][1], board[i][2])) {
+      winner = board[i][0];
+    }
+  }
+
+  // Vertical
+  for (let i = 0; i < 3; i++) {
+    if (equals3(board[0][i], board[1][i], board[2][i])) {
+      winner = board[0][i];
+    }
+  }
+
+  // Diagonal
+  if (equals3(board[0][0], board[1][1], board[2][2])) {
+    winner = board[0][0];
+  }
+  if (equals3(board[2][0], board[1][1], board[0][2])) {
+    winner = board[2][0];
+  }
+
+  // Check for Tie / Open Spots
+  let openSpots = 0;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (board[i][j] == '') {
+        openSpots++;
+      }
+    }
+  }
+
+  if (winner == null && openSpots == 0) {
+    return 'tie';
+  } else {
+    return winner;
+  }
+}
+
+function mousePressed() {
+  if (currentPlayer == human) {
+    // Human makes a turn
+    let i = floor(mouseX / w);
+    let j = floor(mouseY / h);
+    // If valid turn
+    if (board[i][j] == '') {
+      board[i][j] = human;
+      currentPlayer = ai;
+      
+      // Immediately trigger AI after human moves
+      if (checkWinner() == null) {
+        bestMove();
+      }
+    }
+  }
+}
+
+function draw() {
+  background(255);
+  strokeWeight(4);
+
+  // Draw Grid lines
+  line(w, 0, w, height);
+  line(w * 2, 0, w * 2, height);
+  line(0, h, width, h);
+  line(0, h * 2, width, h * 2);
+
+  for (let j = 0; j < 3; j++) {
+    for (let i = 0; i < 3; i++) {
+      let x = i * w + w / 2;
+      let y = j * h + h / 2;
+      let spot = board[i][j];
+      textSize(32);
+      let r = w / 4;
+      if (spot == human) {
+        noFill();
+        ellipse(x, y, r * 2);
+      } else if (spot == ai) {
+        line(x - r, y - r, x + r, y + r);
+        line(x + r, y - r, x - r, y + r);
+      }
+    }
+  }
+
+  let result = checkWinner();
+  if (result != null) {
+    noLoop();
+    let resultP = createP('');
+    resultP.style('font-size', '32pt');
+    if (result == 'tie') {
+      resultP.html('Tie!');
+    } else {
+      resultP.html(`${result} wins!`);
+    }
+  }
+}
